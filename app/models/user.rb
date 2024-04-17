bb33b18ac64cda79dbfd071f1ae99d174b6efed7
ShinjiShiozaki <shinji.shiozaki@nifty.com>
2024-04-17 15:39:33 +0900

    コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :mtweets
   has_many :comments  # commentsテーブルとのアソシエーション
+
+  validates :nickname, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: :show
-  #コメント
-end
+  #コメント、メインで修正、コミットなしで、ブランチを作成し「Leave my changes on master」を選択すると、この説明が消えることを確認する
+  #コメント、メインで修正、コミットした後に、ブランチを作成し「Leave my changes on master」を選択すればこの説明が消えないことを確認する
